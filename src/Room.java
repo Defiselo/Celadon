0e4b3b00008e5457535cb26c6511bcdc0cbd8f59
cabicar <mcabik881@gmail.com>
2025-02-20 11:31:53 +0100

    It dont work :(

--- src/Room.java
+++ src/Room.java
@@ -1,7 +1,7 @@
 public class Room {
     private int ID;
     private String name;
-    private int[] directions = new int[4];
+    private int[] directions;
 
     public Room(int ID, String name, int[] directions) {
         this.ID = ID;
